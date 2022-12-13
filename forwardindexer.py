8e41e8ee5f83d8c87f7b4de64a3934bfbe7f719b
hina426 <hinanaeem426@gmail.com>
2022-12-13 11:34:00 +0500

    Forwardindex implemented

--- forwardindexer.py
+++ forwardindexer.py
@@ -3,4 +3,28 @@ import string
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 
+pstemmer = PorterStemmer()
+forwardindex = {}
+
+with open("airwars.json") as f:
+    data = json.load(f)
+
+with open("lexicon.json") as f:
+    lexicon = json.load(f)
+
+
+for i in data:
+    title = i["title"].split()
+    for word in title:
+        if pstemmer.stem(word) in lexicon.keys():
+            if i["id"] in forwardindex:
+                forwardindex[i["id"]].append(lexicon[pstemmer.stem(word)])
+            else:
+                forwardindex[i["id"]] = [lexicon[pstemmer.stem(word)]]
+        else:
+            pass
+
+
+with open("forewardindex.json", "w") as f:
+    f.write(json.dumps(forwardindex))
 
