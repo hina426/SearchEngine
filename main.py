9fdc5da0e81d4115d67877ee6c752e079700be26
hina426 <hinanaeem426@gmail.com>
2022-12-18 16:55:58 +0500

    Main function

---
+++ main.py
@@ -0,0 +1,10 @@
+from docIDs import *
+from forwardindexer import *
+from invertedindex import *
+from lexicon import *
+
+
+lexicon("airwars.json")
+docIDs("airwars.json")
+forwardIndex("airwars.json")
+invertedIndex()
